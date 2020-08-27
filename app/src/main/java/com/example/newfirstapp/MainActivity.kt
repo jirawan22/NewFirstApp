aa94309d8b57c9de7b427b8d80aea46870a85edc
jirawan.bn <jirawan.bn@rmuti.ac.th>
2020-08-28 01:41:34 +0700

    Add Package Database.kt

--- app/src/main/java/com/example/newfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/newfirstapp/MainActivity.kt
@@ -14,18 +14,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,
             R.layout.activity_main)
 
         drawerLayout = binding.drawerLayout
-
         val navController = this.findNavController(R.id.myNavHostFragment)
-
-        NavigationUI.setupActionBarWithNavController(this,navController, drawerLayout)
-
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
         NavigationUI.setupWithNavController(binding.navView, navController)
     }
+
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
         return NavigationUI.navigateUp(navController, drawerLayout)
---
+++ app/src/main/java/com/example/newfirstapp/database/DatabaseDAO.kt
@@ -0,0 +1,4 @@
+package com.example.newfirstapp.database
+
+interface DatabaseDAO {
+}
